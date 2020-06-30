0dc79fbd3626dff6a34edaeddd36ca0826b97a35
Fernando <a20162001@pucp.edu.pe>
2020-06-29 20:50:14 -0500

    fer

--- src/main/java/sw2/lab6/teletok/controller/PostController.java
+++ src/main/java/sw2/lab6/teletok/controller/PostController.java
@@ -1,10 +1,23 @@
 package sw2.lab6.teletok.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import sw2.lab6.teletok.entity.Post;
+import sw2.lab6.teletok.repository.PostRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PostController {
+
+    @Autowired
+    PostRepository postRepository;
 
     @GetMapping(value = {"", "/"})
     public String listPost(){
@@ -26,9 +39,36 @@ public class PostController {
         return "";
     }
 
-    @GetMapping("/post/{id}")
-    public String viewPost() {
-        return "post/view";
+    @ResponseBody
+    @GetMapping("/post/{id}}")
+    public ResponseEntity viewPost(@PathVariable("id") String idStr,
+                           @RequestParam(value = "token",required = false) String tok) {
+
+        HashMap <String,Object> hashMap = new HashMap<>();
+        HttpStatus estadoHTTP = HttpStatus.BAD_REQUEST;
+
+        try{
+
+            Integer idUser = Integer.parseInt(idStr);
+            Integer token = Integer.parseInt(tok);
+
+            List<Post> postList = postRepository.buscarPostPorIdPostyToken(token,idUser);
+
+            estadoHTTP = HttpStatus.OK;
+
+            return new ResponseEntity(postList,estadoHTTP);
+
+        }catch (NumberFormatException e){
+
+            hashMap.put("estado","error");
+            hashMap.put("msg","El id y el token deben ser numeros!!!");
+
+            return new ResponseEntity(hashMap, estadoHTTP);
+
+        }
+
+
+
     }
 
     @PostMapping("/post/comment")
@@ -39,5 +79,42 @@ public class PostController {
     @PostMapping("/post/like")
     public String postLike() {
         return "";
+    }
+
+
+    @ResponseBody
+    @GetMapping(value = "/product/list",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity obtenerProductos(@RequestParam("query") String query){
+
+        HashMap<String,Object> hashMap = new HashMap<>();
+        HttpStatus estadoHTTP = HttpStatus.BAD_REQUEST;
+
+
+
+
+        if(query=="" || query==null){
+
+            estadoHTTP = HttpStatus.OK;
+
+            return new ResponseEntity(postRepository.findAll(), estadoHTTP);
+
+        }else{
+
+            List<Post> postList = postRepository.buscarPostPorPedazoDeNombre(query,query);
+
+            if(postList.isEmpty()){
+
+                Post[] posts = {};
+                return new ResponseEntity(posts, estadoHTTP);
+
+
+            }else{
+
+                estadoHTTP = HttpStatus.OK;
+
+                return new ResponseEntity(postList,estadoHTTP);
+
+            }
+        }
     }
 }
--- src/main/java/sw2/lab6/teletok/repository/PostRepository.java
+++ src/main/java/sw2/lab6/teletok/repository/PostRepository.java
@@ -1,9 +1,20 @@
 package sw2.lab6.teletok.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import sw2.lab6.teletok.entity.Post;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    @Query(value = "SELECT pos.* FROM teletok.post pos, teletok.user usu WHERE pos.user_id = usu.id AND pos.description = %?1% OR usu.username = %?2%", nativeQuery = true)
+    List<Post> buscarPostPorPedazoDeNombre(String name1, String name2);
+
+
+    @Query(value = "SELECT pos.* FROM teletok.post pos, teletok.user usu, teletok.post_like pl, teletok.token tok WHERE usu.id = pl.user_id AND pl.post_id = pos.id AND tok.user_id = usu.id AND tok.id = 1 AND pos.id = ?2", nativeQuery = true)
+    List<Post> buscarPostPorIdPostyToken(Integer token, Integer idPost);
+
 }
